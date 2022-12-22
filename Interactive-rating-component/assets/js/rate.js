1300c39e2c664dab245b2fe32fe06e74d52ec6e5
Carol Vilar <bo.varig@gmail.com>
2022-12-22 20:07:39 -0300

    insert the form and action to change the pages on submit

---
+++ Interactive-rating-component/assets/js/rate.js
@@ -0,0 +1,16 @@
+const chosenRate = document.getElementsByClassName('rate-btn')
+// changeButtonStyle()
+chosenRate.addEventListener('click', changeButtonStyle)
+
+function changeButtonStyle() {
+  console.log('funfou')
+}
+
+
+
+
+
+
+
+
+// chosenRate.style.cssText = 'color: var(--title-color);' + 'background-color: var(--btn-bg-color);' + 'cursor: pointer;'
